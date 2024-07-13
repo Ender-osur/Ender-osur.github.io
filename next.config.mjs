b091d0391ffbb88bc69cc4a3595266696c5262c5
Ender-osur-cuculi <ender.garizabal@cuculi.com>
2024-07-12 22:38:59 -0500

    fixing unloaded images

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig = {
+  basePath: isProduction ? "/endergarizabal.github.io" : "",
   output:'export',
   distDir:"dist",
   images:{
